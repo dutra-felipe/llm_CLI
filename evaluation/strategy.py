5bc75fd435b932dd71afdddcd064b7aede20f7f8
Felipe Dutra <dutrafelipe77@gmail.com>
2024-11-18 10:38:14 -0300

    updating strategys

--- evaluation/strategy.py
+++ evaluation/strategy.py
@@ -7,18 +7,21 @@ class EvaluationStrategy(ABC):
 
 class LengthEvaluationStrategy(EvaluationStrategy):
     def evaluate(self, response1, response2):
-        score1 = len(response1)
-        score2 = len(response2)
-        return score1, score2
+        return response1 if len(response1) > len(response2) else response2
 
 class KeywordEvaluationStrategy(EvaluationStrategy):
     def __init__(self, keyword):
         self.keyword = keyword
 
     def evaluate(self, response1, response2):
-        count1 = response1.count(self.keyword)
-        count2 = response2.count(self.keyword)
-        return count1, count2
+        count1 = response1.lower().count(self.keyword.lower())
+        count2 = response2.lower().count(self.keyword.lower())
+
+        if count1 > count2:
+            return response1
+        elif count2 > count1:
+            return response2
+        return response2
 
 class CombinedEvaluationStrategy(EvaluationStrategy):
     def __init__(self, strategies):
@@ -28,8 +31,10 @@ class CombinedEvaluationStrategy(EvaluationStrategy):
         score1, score2 = 0, 0
 
         for strategy in self.strategies:
-            strategy_score1, strategy_score2 = strategy.evaluate(response1, response2)
-            score1 += strategy_score1
-            score2 += strategy_score2
+            result = strategy.evaluate(response1, response2)
+            if result == response1:
+                score1 += 1
+            elif result == response2:
+                score2 += 1
 
         return response1 if score1 > score2 else response2
